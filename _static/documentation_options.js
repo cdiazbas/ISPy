aa2e0ecdece8b5c682b347daa1dd054264df8fe7
cdiazbas <cdiazbas@users.noreply.github.com>
2022-01-18 19:27:24 +0000

    deploy: de3f5deb2a1aa8a022f5a13d749fb6c21d08235f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -8,5 +8,6 @@ var DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true
 };
